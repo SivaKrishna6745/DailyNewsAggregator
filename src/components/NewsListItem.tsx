2993c7935459a00eb19fa90566e7a61ff29e46da
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-09-21 21:31:05 +0530

    fix: fix the hover not working on article div

--- src/components/NewsListItem.tsx
+++ src/components/NewsListItem.tsx
@@ -15,7 +15,7 @@ const NewsListItem = ({ article }: NewsListItemProps) => {
             className="m-2 p-3 bg-gray-800 rounded-lg flex justify-between text-xl"
             style={{ color: moodMap[mood].color }}
         >
-            <div className="text-left flex flex-col gap-8 lg:gap-4 lg:group">
+            <div className="text-left flex flex-col gap-8 lg:gap-4 group">
                 <p className="lg:group-hover:text-green-500 font-bold">{article.title}</p>
                 <p className="lg:opacity-0 lg:max-h-0 overflow-hidden transition-[opacity,max-height] duration-300 lg:group-hover:opacity-100 lg:group-hover:max-h-screen lg:group-hover:delay-100">
                     {article.body.trim()}
